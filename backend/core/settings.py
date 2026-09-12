c531d2bfda602d7379eba5784739913be4cd6820
Sainikitha <sainikithasingireddy@gmail.com>
2026-09-12 11:25:56 +0530

    allow deployed frontend origin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -128,4 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://cross-system-reconciliation-frontend.onrender.com",
 ]
